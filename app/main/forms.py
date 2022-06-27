ca429225de83f4eaeb91da77a7a6d815b8bc7509
Tejash <tpatel@misumiusa.com>
2022-06-26 21:58:22 -0500

    New Features Changes

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,4 @@
+from wsgiref.validate import validator
 from flask import request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
@@ -8,6 +9,7 @@ from app.models import User
 
 class EditProfileForm(FlaskForm):
     username = StringField(_l('Username'), validators=[DataRequired()])
+    email = StringField(_l('Email'),validators=[DataRequired()])
     about_me = TextAreaField(_l('About me'),
                              validators=[Length(min=0, max=140)])
     submit = SubmitField(_l('Submit'))
