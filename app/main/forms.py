a24de1b54dc1b278f189cbf9a85363f1346708dd
Tejash <tpatel@misumiusa.com>
2022-06-27 09:54:13 -0500

    Email Validations
    Email Validations

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,14 +2,14 @@ from wsgiref.validate import validator
 from flask import request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import ValidationError, DataRequired, Length
+from wtforms.validators import ValidationError, DataRequired, Length,Email
 from flask_babel import _, lazy_gettext as _l
 from app.models import User
 
 
 class EditProfileForm(FlaskForm):
     username = StringField(_l('Username'), validators=[DataRequired()])
-    email = StringField(_l('Email'),validators=[DataRequired()])
+    email = StringField(_l('Email'),validators=[DataRequired(),Email()])
     about_me = TextAreaField(_l('About me'),
                              validators=[Length(min=0, max=140)])
     submit = SubmitField(_l('Submit'))
