144255378cc03be78906d948270575715121aca2
Tejash <tpatel@misumiusa.com>
2022-08-21 19:10:47 -0500

    Silenece False positives

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,6 +1,7 @@
+
 from flask import Blueprint
 
 
 bp = Blueprint('auth', __name__)
-
+# flake8: noqa
 from app.auth import routes
--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,5 +2,5 @@ from flask import Blueprint
 
 
 bp = Blueprint('main', __name__)
-
+# flake8: noqa
 from app.main import routes
